94c8d8a763a6c94f3f6dc9674e12ef6c5859f39f
Maros Kukan <maros.kukan@me.com>
2021-05-25 12:52:35 +0200

    Replace static item generation with database query

--- src/app.py
+++ src/app.py
@@ -10,7 +10,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    conn = get_db()
+    c = conn.cursor()
+
+    items_from_db = c.execute("""SELECT
+                    i.id, i.title, i.description, i.price, i.image, c.name, s.name 
+                    FROM
+                    items AS i
+                    INNER JOIN categories AS c ON i.category_id = c.id
+                    INNER JOIN subcategories AS s ON i.subcategory_id = s.id
+                    ORDER BY i.id DESC
+    """)
+
+    items = []
+    for row in items_from_db:
+        item = {
+            "id": row[0],
+            "title": row[1],
+            "description": row[2],
+            "price": row[3],
+            "image": row[4],
+            "category": row[5],
+            "subcategory": row[6]
+        }
+        items.append(item)
+
+    return render_template("home.html", items=items)
 
 @app.route('/item/new', methods=["GET", "POST"])
 def new_item():
