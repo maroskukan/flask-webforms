e5ba14a668b2cd5c8acbb86cf0d5fcc47d21340e
Maros Kukan <maros.kukan@me.com>
2021-05-26 12:57:09 +0200

    Add WTForms Validators

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@
 
 from flask import Flask, render_template, request, redirect, url_for, g, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, DecimalField
+from wtforms.validators import InputRequired, DataRequired, Length
 
 import sqlite3
 #import pdb
@@ -14,9 +15,15 @@ app.config['SECRET_KEY'] = "secretkey"
 
 
 class NewItemForm(FlaskForm):
-    title       = StringField("Title")
-    price       = StringField("Price")
-    description = TextAreaField("Description")
+    title       = StringField("Title", validators=[InputRequired("Please fill out this field."),
+                                                   DataRequired("Data is required."),
+                                                   Length(min=5, max=20, message="Input must be between 5 and 20 characters")])
+    price       = DecimalField("Price")
+    description = TextAreaField("Description", validators=[InputRequired("Please fill out this field."),
+                                                           DataRequired("Data is required."),
+                                                           Length(min=5, max=40, message="Input must be between 5 and 40 characters")])
+    category    = SelectField("Category", coerce=int)
+    subcategory = SelectField("Subcategory", coerce=int)
     submit      = SubmitField("Submit")
 
 
@@ -57,7 +64,20 @@ def new_item():
     c = conn.cursor()
     form = NewItemForm()
 
-    if request.method == "POST":
+    c.execute("SELECT id, name FROM categories")
+    categories = c.fetchall()
+    # [(1, 'Food'), (2, 'Technology'), (3, 'Books')]
+    form.category.choices = categories
+
+    c.execute("""SELECT id, name FROM subcategories
+                WHERE category_id = ?""",
+                (1,)
+    )
+    subcategories = c.fetchall()
+    # [(1, 'Food'), (2, 'Technology'), (3, 'Books')]
+    form.subcategory.choices = subcategories
+
+    if form.validate_on_submit():
         # Process the form data
         c.execute("""INSERT INTO items
                     (title, description, price, image, category_id, subcategory_id)
@@ -67,8 +87,8 @@ def new_item():
                       form.description.data,
                       float(form.price.data),
                       "",
-                      1,
-                      1
+                      form.category.data,
+                      form.subcategory.data
                   )
                   )
         conn.commit()
@@ -76,7 +96,9 @@ def new_item():
         flash("Item {} has been successfully submitted".format(
             request.form.get("title")), "success")
         return redirect(url_for("home"))
-
+    
+    if form.errors:
+        flash("{}".format(form.errors), "danger")
     return render_template("new_item.html", form=form)
 
 
