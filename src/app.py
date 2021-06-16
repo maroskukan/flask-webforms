5ebb7169677212ef3fcc4d4fd077501d9133523e
Maros Kukan <maros.kukan@me.com>
2021-06-16 12:27:38 +0200

    Add validator reusability by converting function to class

--- src/app.py
+++ src/app.py
@@ -25,20 +25,45 @@ app.config['ALLOWED_IMAGE_EXTENSIONS'] = ["jpeg", "jpg", "png"]
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16 MB
 app.config['IMAGE_UPLOADS'] = os.path.join(basedir, "uploads")
 
-def belongs_to_category(message):
-    message = message
+class BelongsToOtherFieldOption:
+    def __init__(self, table, belongs_to, foreign_key=None, message=None):
+        if not table:
+            raise AttributeError("""
+            BelongsToOtherFieldOption validator needs the table parameter
+            """)
+        if not belongs_to:
+            raise AttributeError("""
+            BelongsToOtherFieldOption validator needs the belongs_to parameter
+            """)
+        
+        self.table = table
+        self.belongs_to = belongs_to
 
-    def _belongs_to_category(form, field):
+        if not foreign_key:
+            foreign_key = belongs_to + "_id"
+        if not message:
+            message = "Chosen option is not valid."
+
+        self.foreign_key = foreign_key
+        self.message = message
+
+    def __call__(self, form, field):
         c = get_db().cursor()
-        c.execute("""SELECT COUNT(*) FROM subcategories
-                        WHERE id = ? AND category_id = ?""",
-                        (field.data, form.category.data)
-        )
+        try:
+            c.execute("""SELECT COUNT(*) FROM {}
+                        WHERE id = ? AND {} = ?""".format(
+                            self.table,
+                            self.foreign_key
+                        ),
+                        (field.data, getattr(form, self.belongs_to).data)
+            )
+        except Exception as e:
+            raise AttributeError("""
+            Passed parameters are not correct. {}
+            """.format(e))
         exists = c.fetchone()[0]
         if not exists:
-            raise ValidationError(message)
-    
-    return _belongs_to_category
+            raise ValidationError(self.message)
 
 class ItemForm(FlaskForm):
     title       = StringField("Title", validators=[InputRequired("Please fill out this field."),
@@ -52,7 +77,7 @@ class ItemForm(FlaskForm):
 
 class NewItemForm(ItemForm):
     category    = SelectField("Category", coerce=int)
-    subcategory = SelectField("Subcategory", coerce=int, validators=[belongs_to_category("Custom error message")])
+    subcategory = SelectField("Subcategory", coerce=int, validators=[BelongsToOtherFieldOption(table="subcategories", belongs_to="category", message="Subcategory does not belong to that category.")])
     submit      = SubmitField("Submit")
 
 class DeleteItemForm(FlaskForm):
